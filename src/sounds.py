c64760c46359bfee9c03b8c9001f3fa74d3d082c
haakooto <hakonolav.torvik@gmail.com>
2021-02-20 14:28:03 +0100

    sound effects

---
+++ src/sounds.py
@@ -0,0 +1,22 @@
+import numpy as np
+import simpleaudio as sa
+from threading import Thread
+
+
+def play(file):
+    def do(file):
+        wave_obj = sa.WaveObject.from_wave_file("../sounds/" + file)
+        play_obj = wave_obj.play()
+        play_obj.wait_done()
+
+    doer = Thread(target=do, args=(file,))
+    doer.start()
+
+
+def nelson():
+    if np.random.random() < 0.15:
+        play("haha.wav")
+
+
+if __name__ == "__main__":
+    nelson()
