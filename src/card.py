7424e5b5f39f7b84c3fd4cf0ef2e316035964765
haakooto <hakonolav.torvik@gmail.com>
2021-02-20 14:35:59 +0100

    move patches production, allow for custom style

--- src/card.py
+++ src/card.py
@@ -1,13 +1,14 @@
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.offsetbox import DrawingArea
 from matplotlib.transforms import Affine2D
 import matplotlib.patches as mpatches
 import matplotlib.path as mpath
-import sys
 from copy import copy
-import numpy as np
+import sys
 
-Path = mpath.Path  # see 'make_blobs()'
+Path = mpath.Path
+Patch = mpatches.PathPatch
 
 
 class AxBorder(mpatches.Rectangle):
@@ -17,9 +18,9 @@ class AxBorder(mpatches.Rectangle):
     If Axes for deck (last); is hatched and beautiful
     """
 
-    def __init__(self, last=False):
+    def __init__(self, last=False, ec="white"):
         c = (0.01, 0.01)
-        super().__init__(c, 0.98, 0.99, lw=5, fill=False, ec="white")
+        super().__init__(c, 0.98, 0.99, lw=5, fill=False, ec=ec)
         if last:
             self.set_fill(False)
             self.set_color("cyan")
@@ -33,7 +34,32 @@ class Card(DrawingArea):
     The current possible values for the properties are not nessisarily final,
     However, as its only for visualization changes are faaaar down on the priority list
     """
-    def __init__(self, colour, shape, fill, number):
+
+    Rectangle = mpatches.Rectangle((0.25, 0.4), 0.5, 0.2)
+    Ellipse = mpatches.Ellipse((0.5, 0.5), 0.7, 0.2)
+    Rombus = Patch(Path(
+        *list(zip(*[
+                (Path.MOVETO, [0.5, 0.6]),
+                (Path.LINETO, [0.15, 0.5]),
+                (Path.LINETO, [0.5, 0.4]),
+                (Path.LINETO, [0.85, 0.5]),
+                (Path.LINETO, [0.5, 0.6]),
+            ]))[::-1] 
+            ))
+    Arrow = Patch(Path(
+        *list(zip(*[  # Can make any shape, just set coords in unit axes
+                (Path.MOVETO, [0.15, 0.55]),
+                (Path.LINETO, [0.15, 0.45]),
+                (Path.LINETO, [0.65, 0.45]),
+                (Path.LINETO, [0.6, 0.35]),
+                (Path.LINETO, [0.85, 0.5]),
+                (Path.LINETO, [0.6, 0.65]),
+                (Path.LINETO, [0.65, 0.55]),
+                (Path.LINETO, [0.15, 0.55]),
+            ]))[::-1]
+            ))
+
+    def __init__(self, colour, shape, fill, number, cd=None, sd=None):
         super().__init__(1, 1, 0, 0)  # init unit-size DrawingArea
         """
         Colours: 1: green
@@ -59,8 +85,14 @@ class Card(DrawingArea):
         self._id = np.asarray([int(i) for i in self.id])
 
         # Dicts to relate values to readable descriptions
-        self.cd = {1: "green", 2: "blue", 3: "red"}
-        self.sd = {1: "ellipse", 2: "rombus", 3: "arrow"}
+        if cd is None:
+            self.cd = {0: "black", 1: "green", 2: "blue", 3: "red"}
+        else:
+            self.cd = cd
+        if sd is None:
+            self.sd = {0: "rectangle", 1: "ellipse", 2: "rombus", 3: "arrow"}
+        else:
+            self.sd = sd
         self.fd = {1: "none", 2: "hatched", 3: "filled"}
         self.nd = {1: "one", 2: "two", 3: "three"}
 
@@ -72,7 +104,7 @@ class Card(DrawingArea):
         return f"colour: {c}, shape: {s}, fill: {f}, number: {n}"
 
     def __repr__(self):
-        return f"<Set.Card.Card object {self.id}>"
+        return f"<Set.card.Card object {self.id}>"
 
     def is_same(self, other):
         assert isinstance(other, Card)
@@ -100,37 +132,17 @@ class Card(DrawingArea):
 
         patches = []
         # Make correct shape
-        if self.s == 1:  # Ellipse
-            patch = mpatches.Ellipse((0.5, 0.5), 0.7, 0.2)
+        if self.s == 0:  # Rectangle
+            patch = copy(Card.Rectangle)
+        elif self.s == 1:  # Ellipse
+            patch = copy(Card.Ellipse)
         elif self.s == 2:  # Rombus
-            data = [
-                (Path.MOVETO, [0.5, 0.6]),
-                (Path.LINETO, [0.15, 0.5]),
-                (Path.LINETO, [0.5, 0.4]),
-                (Path.LINETO, [0.85, 0.5]),
-                (Path.LINETO, [0.5, 0.6]),
-            ]
-            codes, verts = zip(*data)
-            path = Path(verts, codes)
-            patch = mpatches.PathPatch(path)
-
+            patch = copy(Card.Rombus)
         elif self.s == 3:  # Arrow
-            data = [  # Can make any shape, just set coords in unit axes
-                (Path.MOVETO, [0.15, 0.55]),
-                (Path.LINETO, [0.15, 0.45]),
-                (Path.LINETO, [0.65, 0.45]),
-                (Path.LINETO, [0.6, 0.35]),
-                (Path.LINETO, [0.85, 0.5]),
-                (Path.LINETO, [0.6, 0.65]),
-                (Path.LINETO, [0.65, 0.55]),
-                (Path.LINETO, [0.15, 0.55]),
-            ]
-            codes, verts = zip(*data)
-            path = Path(verts, codes)
-            patch = mpatches.PathPatch(path)
+            patch = copy(Card.Arrow)
 
         patch.set_linewidth(5)
-        patch.set_color(self.cd[self.c]) # Make correct colour
+        patch.set_color(self.cd[self.c])  # Make correct colour
 
         # Make correct fill
         if self.f == 1:
@@ -187,7 +199,7 @@ class Card(DrawingArea):
         return ids
 
 
-if __name__ == "__main__":
+def standard_card():
     # Plot single card for testing
     fig, ax = plt.subplots()
     try:
@@ -197,3 +209,7 @@ if __name__ == "__main__":
     C.make_blobs(ax)
     ax.add_artist(C)
     plt.show()
+
+
+if __name__ == "__main__":
+    standard_card()
