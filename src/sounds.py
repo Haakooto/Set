c974aa5c2743892c381e8a6946af6f6af9afb952
haakooto <hakonolav.torvik@gmail.com>
2022-06-20 23:57:20 +0200

    fix sounds

--- src/sounds.py
+++ src/sounds.py
@@ -5,11 +5,7 @@ from threading import Thread
 
 def play(file):
     def do(file):
-<<<<<<< HEAD
-        wave_obj = sa.WaveObject.from_wave_file("sounds/" + file)
-=======
         wave_obj = sa.WaveObject.from_wave_file("./sounds/" + file)
->>>>>>> d09ee44a437790c55b4d15eb0da6a9d14e614efb
         play_obj = wave_obj.play()
         play_obj.wait_done()
 
@@ -18,7 +14,7 @@ def play(file):
 
 
 def nelson():
-    if np.random.random() < 0.05:   
+    if np.random.random() < 0.05:
         play("haha.wav")
 
 
